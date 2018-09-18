cb77e8bec7f1cf6336d239d340838187383707dc
Xavi Vives <xavings@gmail.com>
2018-09-18 17:14:58 +0200

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import PtsCanvas from "./PtsCanvas"
+import { ChartExample, AnimationExample } from './PtsExamples';
+import './App.css';
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            variance: 0.2,
+            pauseAnimation: false
+        }
+
+        this.mockData(0.2);
+
+    }
+
+    mockData(variance) {
+        let gaussian = (x) => {
+            let mean = 0;
+            return (1 / Math.sqrt(2 * Math.PI * variance)) * Math.exp(-(x - mean) * (x - mean) / (2 * variance));
+        };
+
+        this.chartData = [];
+        for (let i = -5; i < 5; i += 0.1) {
+            this.chartData.push(gaussian(i));
+        }
+    }
+
+    handleChange(event) {
+        this.setState({ variance: event.target.value });
+    }
+
+    handleClick(event) {
+        this.setState({ pauseAnimation: !this.state.pauseAnimation });
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        this.mockData(nextState.variance);
+    }
+
+    render() {
+        return (
+
+            <div className="App">
+
+                <div className="row">
+                    <div><ChartExample name="pts_chart" background="#0c9" loop={false} data={this.chartData} variance={this.state.variance} /></div>
+                    <div>
+                        <h3>ChartExample Component</h3>
+                        <p><label>Variance: <input type="range" value={this.state.variance} min={0.05} max={5} step={0.05} onChange={this.handleChange.bind(this)} /></label> ({this.state.variance})</p>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}

fdda025635fc38b42222ceac879d1197fb5500e5
Xavi <xavings@gmail.com>
2018-10-02 05:48:58 +0000

    Better link checks using CIDS and CIDTool

--- src/LinkType.js
+++ src/LinkType.js
@@ -1,21 +1,32 @@
+const CID = require('cids')
+const CIDTool = require('cid-tool')
+
 export default class LinkType {
     constructor(obj) {
         if (!LinkType.isLink(obj))
             throw (new Error('Object is not a valid link', obj))
-        this._link = obj
+
+        this._link = CIDTool.format(obj['/'])
     }
 
-    get link () {
-        return obj['/']
+    get link() {
+        return _link
     }
 
     static isLink(obj) {
-        if(!obj)
+        if (!obj)
             return false
-            
+
         if (!obj['/'])
             return false
-        //Anything in the link is valid for now...
+
+        try {
+            let cid = new CID(obj['/'])
+        }
+        catch (err) {
+            return false
+        }
+
         return true
     }
 }

9c24b4d9b7e3a2e42c960322e21852375835d94f
Xavi <xavings@gmail.com>
2018-10-04 20:24:33 +0000

    Extending burl

--- src/Burl.js
+++ src/Burl.js
@@ -1,24 +1,34 @@
+const OTYPE = {
+    UNDEFINED : 'undefined',
+    TEXT : 'text',
+    IMAGE : 'image'
+}
+
+
 export default class Burl{
+
     constructor(oid, pt)
     {
         this._oid = oid
         this._pt = pt
         this._nodes = []
+        this._fileExtension = OTYPE.UNDEFINED
+        this._file = null
     }
 
-    get origin()
+    get oid()
     {
-        return _oid
+        return this._oid
     }
 
     get pt ()
     {
-        return _pt
+        return this._pt
     }
 
     get nodes()
     {
-        return _nodes
+        return this._nodes
     }
 
     addNode(n)
@@ -26,4 +36,29 @@ export default class Burl{
         this._nodes.push(n)
     }
 
+    set file(file)
+    {
+        this._file = file
+    }
+
+    get file()
+    {
+        return this._file
+    }
+
+    get preview()
+    {
+        return this._file.toString('utf-8')
+    }
+
+    get hasPreview()
+    {
+        if(this._file)
+            return true
+        return false
+    }
+
+
+
 }
+

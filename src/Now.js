4f78c8e9a0a6ba61a6aec9b488cdf101ab566317
Xavi <xavings@gmail.com>
2018-10-05 20:31:29 +0000

    Holding states for selected burls

--- src/Now.js
+++ src/Now.js
@@ -20,9 +20,38 @@ export default class Now {
     {
         Now._zoom = z
     }
+
+    static get currentBurl()
+    {
+        return this._currentBurl
+    }
+
+    static set currentBurl(burl)
+    {   
+        this._currentBurl = burl
+    }
+
+
+    static get startBurl()
+    {
+        return this._startBurl
+    }
+
+    static set startBurl(burl)
+    {   
+        this._startBurl = burl
+    }
+
+
 }
 
 Now._zoom = 1
 Now._originRadius = 50
 Now._nodeRadius = Now._originRadius * 1.2
 Now._nodeArm = 100
+
+Now._isPressing = false
+
+Now._currentBurl = null
+Now._startBurl = null
+Now._startNode = null

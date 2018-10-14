030cbe256e035e9da209f898f7318dc3a49913b9
Xavi <xavings@gmail.com>
2018-10-14 16:14:05 +0000

    Race condition was preventing old nodes to be removed

--- src/index.js
+++ src/index.js
@@ -107,8 +107,8 @@ export default class IPLDReodeder extends PtsCanvas {
             this.ipfsController.loadDag(cid, (data) => {
 
                 if (NodeType.isNode(data)) {
-                    this.createNode(data, cid)
                     this.onCidLoaded(cid)
+                    this.createNode(data, cid)
                 }
                 else {
                     this.createIPLD(data, cid)
@@ -402,7 +402,8 @@ export default class IPLDReodeder extends PtsCanvas {
         this.paintAll()
         this.paintFocusTree(Now.hoverSelection)
         this.paintBorningRelation()
-        this.world.drawParticles((p, i) => { this.form.strokeOnly('#9993').point(p, p.radius, "circle") });
+        this.world.drawParticles((p, i) => { console.log(i)
+            this.form.strokeOnly('#9993').point(p, p.radius, "circle") });
 
         /*for (let pt of this._ptsToDraw)
             this.paint.bubble(pt, 10, '#f36')

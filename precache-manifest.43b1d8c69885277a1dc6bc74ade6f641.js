cd5681ac6a83098cbe0561c58ffbcc965d6e4b00
Xavi <xavings@gmail.com>
2018-10-22 14:07:30 +0000

    Updates

---
+++ precache-manifest.43b1d8c69885277a1dc6bc74ade6f641.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ea40fbf74c312d04327",
+    "url": "/mind-map/static/js/runtime~main.1ea40fbf.js"
+  },
+  {
+    "revision": "360a1bd33745c414dfcf",
+    "url": "/mind-map/static/js/main.360a1bd3.chunk.js"
+  },
+  {
+    "revision": "6c0b128aee1f17979b53",
+    "url": "/mind-map/static/js/1.6c0b128a.chunk.js"
+  },
+  {
+    "revision": "360a1bd33745c414dfcf",
+    "url": "/mind-map/static/css/main.ec95b798.chunk.css"
+  },
+  {
+    "revision": "961107652d0ab1233ee74e56bbd4db99",
+    "url": "/mind-map/index.html"
+  }
+];
